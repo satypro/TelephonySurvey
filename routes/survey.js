6fa798e2f509ad99017cf14d1718ab7de3928c55
Satendra Singh <satendra_singh@live.com>
2017-04-22 01:53:26 +0530

    first commit

---
+++ routes/survey.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  
+    let number = req.body.number;
+
+    // now make the call to the Telephony Gateway  ex. exotel
+
+    
+});
+
+module.exports = router;
